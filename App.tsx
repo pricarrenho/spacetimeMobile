99b32d30ee15f2891bae379c32abd87faea80f06
Priscilla Carrenho <priscillaonil@gmail.com>
2023-05-18 19:19:15 -0300

    feat: add authorization

--- App.tsx
+++ App.tsx
@@ -1,20 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import blurBg from "./src/assets/bg-blur.png";
+import Stripes from "./src/assets/stripes.svg";
+import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
+import { styled } from "nativewind";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { useEffect } from "react";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { api } from "./src/lib/api";
+
+const StyledStripes = styled(Stripes);
+
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/fad59670905738419c5f",
+};
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: "fad59670905738419c5f",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nlwspacetime",
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      console.log("1");
+      const { code } = response.params;
+      console.log("2", code);
+
+      api
+        .post("/register", {
+          code,
+        })
+        .then((response) => {
+          console.log("entrou");
+          console.log("3");
+          const { token } = response.data;
+
+          console.log(token);
+        })
+        .catch((error) => {
+          console.log("4");
+          console.log({ error });
+        });
+    }
+  }, [response]);
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembranças
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 por Priscilla Carrenho no NLW da Rocketseat
+      </Text>
+
+      <StatusBar style="light" />
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
