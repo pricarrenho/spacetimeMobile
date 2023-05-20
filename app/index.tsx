393debf74ca9e2224b7fce46f14ff5c4923ba065
Priscilla Carrenho <priscillaonil@gmail.com>
2023-05-20 15:28:00 -0300

    feat: add memories

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
-import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-import blurBg from "./src/assets/bg-blur.png";
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
-import { styled } from "nativewind";
-import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { useEffect } from "react";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import * as SecureStore from "expo-secure-store";
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { api } from "./src/lib/api";
+import { api } from "../src/lib/api";
 
-const StyledStripes = styled(Stripes);
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
 
 const discovery = {
   authorizationEndpoint: "https://github.com/login/oauth/authorize",
@@ -25,11 +16,7 @@ const discovery = {
 };
 
 export default function App() {
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  });
+  const router = useRouter();
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
@@ -42,42 +29,28 @@ export default function App() {
     discovery
   );
 
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post("/register", {
+      code,
+    });
+
+    const { token } = response.data;
+
+    await SecureStore.setItemAsync("token", token);
+
+    router.push("/memories");
+  }
+
   useEffect(() => {
     if (response?.type === "success") {
-      console.log("1");
       const { code } = response.params;
-      console.log("2", code);
 
-      api
-        .post("/register", {
-          code,
-        })
-        .then((response) => {
-          console.log("entrou");
-          console.log("3");
-          const { token } = response.data;
-
-          console.log(token);
-        })
-        .catch((error) => {
-          console.log("4");
-          console.log({ error });
-        });
+      handleGithubOAuthCode(code);
     }
   }, [response]);
 
-  if (!hasLoadedFonts) {
-    return null;
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: "absolute", left: "-100%" }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className=" flex-1 items-center  px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
@@ -105,8 +78,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 por Priscilla Carrenho no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" />
-    </ImageBackground>
+    </View>
   );
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.15.12:3333",
+  baseURL: "http://192.168.15.15:3333",
 });
